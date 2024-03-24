7b27487df3b1df17d86345c5258729cd0ecc3302
siddhanthakur43 <siddhantthakur43@gmail.com>
2024-03-24 17:57:44 +0530

    Nested div

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
-const heading = React.createElement('h1', {
-    id: 'heading'
-}, 'Hello World From React!!');
-console.log(heading);
+/**
+ * Nested div
+ <div id='parent'>
+    <div id='child1'>
+       <h1>I'm an h1 tag!</h1>
+       <h2>I'm an h2 tag!</h2>
+    </div>
+    <div id='child2'>
+       <h1>I'm an h1 tag!</h1>
+       <h2>I'm an h2 tag!</h2>
+    </div>
+ </div>
+ * */
+
+const parent = React.createElement('div', {id: 'parent'},
+ [React.createElement('div', {id: 'child1'},
+  [React.createElement('h1', {}, "I'm an h1 tag!!"),
+   React.createElement('h2', {}, "I'm an h2 tag!!")]),
+   React.createElement('div', {id: 'child2'},
+   [React.createElement('h1', {}, "I'm an h1 tag!!"),
+    React.createElement('h2', {}, "I'm an h2 tag!!")])]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(parent);
---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+h1 tag using html
+h1 tag using javascript
+React cdn 
+h1 tag using core react
+React.createElement takes 3 arguments 
+    1) Name of tag
+    2) Object which take attributes ex. prop
+    3) Children if single child then directly put and if multiple use array
+React.createElement at the end is an object => HTML(browser understands)
+render puts html child inside root
+render replaces the whole code inside the root
