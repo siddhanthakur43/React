051700a79672e9074843f94e28759b8b44818904
siddhanthakur43 <siddhantthakur43@gmail.com>
2024-03-29 01:41:22 +0530

    Episode 2

--- App.js
+++ App.js
@@ -1,16 +1,5 @@
-/**
- * Nested div
- <div id='parent'>
-    <div id='child1'>
-       <h1>I'm an h1 tag!</h1>
-       <h2>I'm an h2 tag!</h2>
-    </div>
-    <div id='child2'>
-       <h1>I'm an h1 tag!</h1>
-       <h2>I'm an h2 tag!</h2>
-    </div>
- </div>
- * */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement('div', {id: 'parent'},
  [React.createElement('div', {id: 'child1'},
