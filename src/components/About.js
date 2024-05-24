cdf58077782d0ce6b3a73237980080f15e751982
siddhanthakur43 <siddhantthakur43@gmail.com>
2024-05-25 00:46:46 +0530

    episode 8 part 2

--- src/components/About.js
+++ src/components/About.js
@@ -5,20 +5,16 @@ import UserClass from './UserClass'
 class About extends React.Component{
   constructor(props) {
     super(props)
-    console.log('Parent Constructor');
   }
   componentDidMount() {
-    console.log('Parent componentDidMount');
   }
 
   render() {
-    console.log('Parent Render');
     return (
       <div>
       <h1>About</h1>
       <div>
           <UserClass name={'Siddhant'} location={'Jabalpur'} />
-          <UserClass name = {'Elon'} location = {'NewYork'} />
       </div>
       </div>
   )
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,34 +4,55 @@ class UserClass extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 0,
+            userInfo:{
+            name: 'dummy Name',
+            location: 'dummy location',
+            avatar_url: 'avatar_url'}
         }
-        console.log( this.props.name,'child constructor');
+        console.log('constructor');
     }
-    componentDidMount() {
-        console.log(this.props.name, 'child componentDidMount');
+    async componentDidMount() {
+        console.log('componentDidMount');
+        const data = await fetch('https://api.github.com/users/siddhanthakur43');
+        const json = await data.json();
+        this.setState({
+            userInfo: json,
+        })
+    }
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+
+    }
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
     }
     render() {
-        console.log(this.props.name, 'child render');
-        const { name, location } = this.props;
-        const { count } = this.state;
-        return (<div className='user-info'>
-            <h1>Count = {count}</h1>
-            <button onClick={() => {
-                this.setState({
-                    count:this.state.count + 1
-                })
-            }}>Inc Count</button>
-            <button onClick={() => {
-                this.setState({
-                    count: this.state.count - 1
-                })
-            }}>Dec Count</button>
-      <h1>Name: {name}</h1>
-      <h3>Location: {location}</h3>
-          <h3>Contact: siddhantthakur43@gmail.com</h3>
-      </div>)
+        const { name, location, avatar_url } = this.state.userInfo;
+        console.log('render');
+        return (
+            <div className='user-info'>
+                <h1>Name: {name}</h1>
+                <h3>Location: {location}</h3>
+                <img src={avatar_url}></img>
+                <h3>Contact: siddhantthakur43@gmail.com</h3>
+            </div>
+        );
     }
 }
 
-export default UserClass
+export default UserClass;
+
+/*
+Mounting Cycle:-
+contructor(dummy data)
+render(dummy data)
+<html dummy data>
+componentDidMount
+Api call
+this.setState(Api data)
+
+Update Cycle:-
+render(API Data)
+<html api data>
+ComponentDidUpdate
+*/
